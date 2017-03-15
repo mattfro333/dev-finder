e879d9edd290125c31ea85313c194ae0aa7bdcb2
Niall Cummins <niall.cummins1@gmail.com>
2017-03-15 10:27:46 -0600

    Added primary keys

--- server/db/init/create_tables.sql
+++ server/db/init/create_tables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS applications(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 user_id INTEGER,
 job_id INTEGER,
 timestamp Date
 );
 
 CREATE TABLE IF NOT EXISTS company_reviews(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 company_id INTEGER,
 rating INTEGER,
 user_id INTEGER,
@@ -14,7 +14,7 @@ review TEXT
 );
 
 CREATE TABLE IF NOT EXISTS companys(
-company_id INTEGER,
+company_id INTEGER SERIAL PRIMARY KEY,
 name VARCHAR(50),
 description TEXT,
 industry VARCHAR(20),
@@ -26,7 +26,7 @@ founded INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS devs(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 profilepic VARCHAR(500),
 type VARCHAR(20),
 description TEXT,
@@ -41,7 +41,7 @@ state VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS experience(
-experience_id INTEGER,
+experience_id INTEGER SERIAL PRIMARY KEY,
 title VARCHAR(100),
 description TEXT,
 work_true_education_false BOOLEAN,
@@ -49,26 +49,26 @@ user_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS experience_skills(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 experience_id INTEGER,
 skill_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS flagged_jobs(
-flag_id INTEGER,
+flag_id INTEGER SERIAL PRIMARY KEY,
 user_id INTEGER,
 job_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS job_skills(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 job_id INTEGER,
 skill_id INTEGER,
 
 );
 
 CREATE TABLE IF NOT EXISTS jobs(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 company_id INTEGER,
 job_description TEXT,
 job_title TEXT,
@@ -79,13 +79,13 @@ timestamp DATE
 );
 
 CREATE TABLE IF NOT EXISTS message_room(
-room_id INTEGER,
+room_id INTEGER SERIAL PRIMARY KEY,
 user1_id INTEGER,
 user2_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS messages(
-message_id INTEGER,
+message_id INTEGER SERIAL PRIMARY KEY,
 message TEXT,
 sender_id INTEGER,
 recipient_id INTEGER,
@@ -93,7 +93,7 @@ room_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS portfolio_pieces(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 user_id INTEGER,
 title VARCHAR(50),
 description TEXT,
@@ -102,36 +102,37 @@ link_url VARCHAR(500)
 );
 
 CREATE TABLE IF NOT EXISTS portfolio_skills(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 portfolio_piece_id INTEGER,
 skill_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS skills(
-skill_id INTEGERskill_name VARCHAR(50);
+skill_id INTEGER SERIAL PRIMARY KEY,
+skill_name VARCHAR(50);
 icon_url VARCHAR(500)
 );
 
 CREATE TABLE IF NOT EXISTS user_companies(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 user_id INTEGER,
 company_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS user_experience(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 user_id INTEGER,
 experience_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS user_skills(
-id INTEGER,
+id INTEGER SERIAL PRIMARY KEY,
 user_id INTEGER,
 skill_id INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS users(
-user_id INTEGER,
+user_id INTEGER SERIAL PRIMARY KEY,
 username VARCHAR(50);
 password VARCHAR(50);
 company BOOLEAN
