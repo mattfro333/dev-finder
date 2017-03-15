79ef03cad0873764052cd43149b243cdaacfbd43
Niall Cummins <niall.cummins1@gmail.com>
2017-03-15 09:02:11 -0600

    Completed some stuff, dudes

--- src/components/profile/devProfile/devProfile.js
+++ src/components/profile/devProfile/devProfile.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
 import { getprofile } from '../../../services/devProfile'
 import {Menu} from 'semantic-ui-react'
-import './devProfile.scss';
+import './devProfile.css';
 
 class DevProfile extends Component{
 
