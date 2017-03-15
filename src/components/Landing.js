8c0bd0a1c904329e485784be7f8f7b560016bee5
mattfro333 <mattfro333@gmail.com>
2017-03-15 15:36:52 -0600

    Landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {browserHistory} from 'react-router';
 import {Input, Button, Container, Header, Segment, Divider, Icon, Image} from 'semantic-ui-react'
 import axios from 'axios'
-
+import './Landing.css';
 
 
 class Landing extends Component{
@@ -19,20 +19,22 @@ class Landing extends Component{
       <Icon name='users' circular />
     </Header>
       <Container text>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong.
-         Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-         Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. </p>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong.
-        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
-        pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequa</p>
+        <p>Dev-Finder is a comprehensive approach designed to connect developers to the companies that fit their skills best, ultimately maximizing efficiency and productivity. This site allows
+        developers and companies to create unique profiles. The idea is that developers can search through company profiles and communicate with them while companies can simultaneously to the same.
+        Our goal is to weed out all the flashy distractions that most social networking mediums have and just leave a seemless line of communication aimed at matching developers with companies
+        that accent their strengths. This will bolster the companies worth and output.</p>
+        <p>Some of the higlights of our site Dev-Finder are a responsive watchlist, quick messaging, e-mailing capabilities, google analytics, and grouping of specific skill sets. We promise a simple solution
+        to the issue of find an employee or an employer. So signup, login, and find
+        the company or developer that best fits you!</p>
       </Container>
 
-
-  <Segment padded>
-    <Button primary fluid content='Login' onClick={()=>browserHistory.push('/login')} />
-    <Divider horizontal>Or</Divider>
-    <Button secondary fluid content='Sign Up' onClick={()=>browserHistory.push('/signup')} />
+<div id='landsignbutton'>
+  <Segment className='landingbuttoncontainer' padded>
+    <Button id='landlogbutton' color="teal" content='Login' onClick={()=>browserHistory.push('/login')} />
+    <Divider vertical>Or</Divider>
+    <Button color="orange" content='Sign Up' onClick={()=>browserHistory.push('/signup')} />
   </Segment>
+</div>
 
       </div>
     )
