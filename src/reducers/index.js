79ebf95350b0f000212e900a551a8b03ddbb36d9
RyanColton <ryan.m.colton@gmail.com>
2017-03-16 16:49:30 -0600

    finished nav bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import {routerReducer} from 'react-router-redux'
 import user from './user'
+import profile from './profile'
 const DevFinder = combineReducers({
+  profile,
   user,
   routing: routerReducer
 })
--- src/store.js
+++ src/store.js
@@ -6,8 +6,8 @@ import {browserHistory} from 'react-router'
 import rootReducer from './reducers/index'
 
 const defaultState ={
-    profile: [[{}],[{}],[{}],[{}]]
-
+  user: {},
+  profile: {}
 }
 
 const store = createStore(rootReducer, defaultState)
