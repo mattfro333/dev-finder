b76fcd12347b46b56d87a0b988417e53ffd72bd0
Niall Cummins <niall.cummins1@gmail.com>
2017-03-20 09:00:59 -0600

    Fixed bug in job page, and another bug in messaging page

--- server/db/jobs/get.sql
+++ server/db/jobs/get.sql
@@ -1,4 +1,5 @@
-SELECT j.* FROM jobs j
-JOIN companys c
+SELECT * FROM jobs 
+JOIN companys 
 ON jobs.company_id = companys.company_id
 where LOWER(job_title)  like LOWER($1);
+-- Don't change this again! It broke last time. 
