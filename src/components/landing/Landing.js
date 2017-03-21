9617929cc6d1f00f5ea1b1b6e81fcaa62fe68878
RyanColton <ryan.m.colton@gmail.com>
2017-03-21 11:22:46 -0600

    fixing my landing and login pages

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -12,36 +12,7 @@ class Landing extends Component{
 
 
   render(){
-<<<<<<< HEAD
-    return(
 
-      <div>
-        <div className="landingbody">
-          <Segment inverted>
-              <Divider inverted />
-              <Divider horizontal inverted id="landtitle">Dev-Finder</Divider>
-          </Segment>
-          <Header textAlign='center'>
-            <Image src={Logo} size='large'/>
-          </Header>
-          <Container text>
-            <p>Dev-Finder is designed to connect developers to companies. This site allows developers and companies to both create profiles. The idea is that developers can search through company profiles and communicate with them and vice versa.
-            Our goal is to weed out all the flashy distractions that most social networking mediums have, and just leave a seemless line of communication aimed at matching developers with companies
-            that accent their strengths. This will bolster the companies worth and output.</p>
-            <p>Some of the highlights of Dev-Finder are a responsive watchlist, quick messaging, e-mailing capabilities, google analytics, and grouping of specific skill sets. We promise a simple solution
-            to the issue of finding an employee, or an employer. So signup, login, and find
-            the company, or developer that best fits you!</p>
-          </Container>
-          <Canvas />
-          <div id='landsignbutton'>
-            <Segment className='landingbuttoncontainer' padded>
-              <Button id='landlogbutton' color="teal" content='Login' onClick={()=>browserHistory.push('/login')} />
-              <Divider vertical>Or</Divider>
-              <Button color="orange" content='Sign Up' onClick={()=>browserHistory.push('/signup')} />
-            </Segment>
-          </div>
-          </div>
-=======
     return(<div>
      <div className= 'land-nav'>
        <div className='logo'></div>
@@ -57,12 +28,11 @@ class Landing extends Component{
         <h2>By Developers. For Developers.</h2>
         <h3>Sign up today to find your next employment opportunity.</h3>
           </div>
-    
+
 
         </div>
->>>>>>> master
       </div>
-      </div>
+     </div>
     )
   }
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -10,13 +10,6 @@ class Login extends Component {
         super()
         this.state = {}
         this.login = this.login.bind(this)
-        this.getSkills = this.getSkills.bind(this)
-    }
-    getSkills = ()=>{
-      return axios.get('/api/skills').then((r)=>{
-         let skills = r.data
-         this.props.addSkills(skills)
-      })
     }
     login = () => {
         return axios.post('/api/login', {
@@ -55,54 +48,31 @@ class Login extends Component {
         return (
 
             <div>
-            <div className="loginMove"> // logTitle (Matt Class)
+            <div className="loginMove">
                 <h1 className="loginMove">Login</h1>
                 <Input className="loginMove" placeholder='Username' onChange={(e) => this.usernameInput = e.target.value}/>
                 <Input className="loginMove"  type='password' placeholder='Password' onChange={(e) => this.passwordInput = e.target.value}/>
                 <Button className="loginMove"  content='Login' color="teal" onClick={() => this.login()}/>
             </div>
-<<<<<<< HEAD
-            <footer className="logfooter">
-                <div className='footerContainer'>
-                <footer className="logfooter">
-                <Segment inverted className="logfooter">
-=======
                 <div className='footerContainer'>
                 <footer className="logfooter">
                 <Segment inverted className="logfooter">
 
->>>>>>> master
 
                   <Divider  color="orange" horizontal inverted className='dottitle' >.</Divider>
 
                 </Segment>
-<<<<<<< HEAD
-               </footer>
-                <Header textAlign='center'>
-                    <Image className="loginvert" src={Logo} size='tiny'/>
-                </Header>
-              </div>
-                </footer>
-
-
-                    <Image className="loginvert" src={Logo} size='tiny'/>
-=======
                 </footer>
 
 
                     <Image className="loginvert" src={Logo} size='tiny'/>
 
                   </div>
->>>>>>> master
 
      </div>
 
         )
     }
-    componentDidMount(){
-      this.getSkills()
-    }
-
 }
 
 export default Login
