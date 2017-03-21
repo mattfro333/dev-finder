0243e0a07d3171cae511dbd14172ea030fc2b897
RyanColton <ryan.m.colton@gmail.com>
2017-03-21 15:02:08 -0600

    added the skills dropdown to the job creation page

--- server/controllers/createjob.js
+++ server/controllers/createjob.js
@@ -4,7 +4,7 @@ module.exports = {
   post: function(req, res){
     var user_id=req.session.passport.user.user_id;
     var filled = 'false'
-    db.createjob.post([user_id,req.body.jobtitle, req.body.jobdesc, req.body.location, req.body.time, filled], function(err, results){
+    db.createjob.post([user_id,req.body.jobtitle, req.body.jobdesc, req.body.location, req.body.time, filled, req.body.skills], function(err, results){
       if (err){
         console.error(err);
         return res.send(err);
