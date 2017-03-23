dbf0dcceeb0cfbf80830212df701766cbdcbd662
Ryan Clark <Rymaster95@gmail.com>
2017-03-23 11:55:15 -0600

    added all functionality that dev profile had to company profile

--- server/controllers/companyProfileCtrl.js
+++ server/controllers/companyProfileCtrl.js
@@ -9,5 +9,23 @@ module.exports = {
       }
       res.send(results);
     })
+  },
+  get_jobs: function(req, res){
+    db.companyProfile.get_jobs([req.body.id], function(err, results){
+      if (err){
+        console.error(err);
+        return res.send(err);
+      }
+      res.send(results);
+    })
+  },
+  update_company: function(req, res){
+    db.companyProfile.update_company([req.body.id, req.body.name, req.body.city, req.body.state, req.body.founded, req.body.bio], function(err, results){
+      if (err){
+        console.error(err);
+        return res.send(err);
+      }
+      res.send(results);
+    })
   }
 }
