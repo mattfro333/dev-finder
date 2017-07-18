40f5d486fc077c5c1e530933be731850331e31fe
mattfro333 <mattfro333@gmail.com>
2017-07-18 10:37:03 -0600

    y

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ const expectedHostname = 'http://devfinder.s3.amazonaws.com';
 const expectedMinSize = 0;
 const expectedMaxSize = null;
 let s3
-aws.process.env.update({
-    accessKeyId: serverPublicKey,
-    secretAccessKey: serverSecretKey
-});
+// aws.update({
+//     accessKeyId: serverPublicKey,
+//     secretAccessKey: serverSecretKey
+// });
 s3 = new aws.S3()
 
 //Set up Session
