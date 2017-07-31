8cbf4a970204ff4d49f10a638f3259715521faa7
mattfro333 <mattfro333@gmail.com>
2017-07-31 08:45:03 -0600

    Square 1

--- server/config.js
+++ server/config.js
@@ -1,8 +1,11 @@
 module.exports = {
   massiveUri:'postgres://lgmqdgnl:LUdviWpCcoSOVmiCDDgBhGAKqRLepY8g@pellefant.db.elephantsql.com:5432/lgmqdgnl',
   port: 3500,
-  SESSION_SECRET:'qwer-asdftyhdfa-fdsdfggec',
-  sercretKey:'+qFrKXNawNn4qLxkBzAALoBeQlbCx9EqNBuECrLg',
-  accessKey:'AKIAIHVU2DJYCPVOMXPA'
+  SESSION_SECRET:'qwer-asdftyhdfa-fdsdfggec'
+
 
 }
+
+
+// sercretKey:'+qFrKXNawNn4qLxkBzAALoBeQlbCx9EqNBuECrLg',
+// accessKey:'AKIAIHVU2DJYCPVOMXPA'
