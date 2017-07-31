323ee2392b65d87cccecc3c91bbf9ae7f5b454f4
mattfro333 <mattfro333@gmail.com>
2017-07-31 09:00:37 -0600

    3

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   massiveUri:'postgres://lgmqdgnl:LUdviWpCcoSOVmiCDDgBhGAKqRLepY8g@pellefant.db.elephantsql.com:5432/lgmqdgnl',
   port: 3500,
-  SESSION_SECRET:'qwer-asdftyhdfa-fdsdfggec'
+  SESSION_SECRET:'qwer-asdftyhdfa-fdsdfggec',
 
 
 }
